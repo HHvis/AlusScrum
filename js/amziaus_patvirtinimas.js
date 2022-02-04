e0f5418f475aa4635f7bc14e22ac984aba21890f
paulius1230 <96494731+paulius1230@users.noreply.github.com>
2022-02-04 09:19:38 +0200

    Update amziaus_patvirtinimas.js

--- js/amziaus_patvirtinimas.js
+++ js/amziaus_patvirtinimas.js
@@ -8,7 +8,7 @@ let tekstas_2 = document.querySelector(".tekstas_2");
     });
     
     mygtukasNe.addEventListener("click", function (){
-        tekstas.innerText = "Jums turi būti 20 ar daugiau metų, kad galėtumėte naudotis svetaine";
+        tekstas.innerText = "Norėdami peržiūrėti šios svetainės turinį, turite būti pilnametis (-ė).";
         tekstas_2.style.display = "none";
         mygtukasTaip.style.display = "none";
         mygtukasNe.style.display = "none";
